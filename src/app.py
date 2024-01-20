08281f0476f9618c0aa654a6ef15853c0312bed0
Yoonjung Choi <gracechoi1357@gmail.com>
2024-01-20 17:10:35 -0500

    add input_to_index for both bread and s&p

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-def input_to_index(year, month):
+def input_to_index_bread(year, month):
     # Start year is 2017
     start_year = 2017
 
@@ -8,4 +8,14 @@ def input_to_index(year, month):
     # The index starts at 1, so add 1 to the result
     return months_since_start + 1
 
-print(input_to_index(2030, 1))
+def input_to_index_sp(year, month):
+    # Start year is 2009
+    start_year = 2009
+
+    # Calculate the number of months since February 2009
+    months_since_start = (year - start_year) * 12 + (month - 1)
+
+    return months_since_start
+
+# print(input_to_index_sp(2023, 11))
+# print(input_to_index_bread(2022, 12))

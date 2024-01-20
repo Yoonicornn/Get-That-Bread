4a0761981bf4e0d8c0cb1885604e82c37238086c
TonyMa8 <antma2003@gmail.com>
2024-01-20 17:51:22 -0500

    flask kinda works

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 
-app = Flask(__name__)
+app=Flask(__name__,template_folder='template')
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Home"
+    return render_template("index.html")
 
+@app.route("/greet", methods=["GET", "POST"])
+def greet():
+    name = request.args.get("name","world")
+    return render_template("greet.html", name=name)
 
 
 if __name__ == "__main__":
